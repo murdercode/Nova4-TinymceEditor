572266b37a0227bd975f306aad1ef64e6ed2ddab
Stefano Novelli <murdercode@gmail.com>
2022-08-30 16:15:35 +0200

    Pint

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -16,12 +16,12 @@ class FieldServiceProvider extends ServiceProvider
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
-            Nova::script('tinymce-editor', __DIR__ . '/../dist/js/field.js');
-            Nova::style('tinymce-editor', __DIR__ . '/../dist/css/field.css');
+            Nova::script('tinymce-editor', __DIR__.'/../dist/js/field.js');
+            Nova::style('tinymce-editor', __DIR__.'/../dist/css/field.css');
         });
 
         $this->publishes([
-            __DIR__ . '/../config/nova-tinymce-editor.php' => config_path('nova-tinymce-editor.php'),
+            __DIR__.'/../config/nova-tinymce-editor.php' => config_path('nova-tinymce-editor.php'),
         ], 'config');
     }
 
