34703bcb361265f2265cd454a033986ebbcd9cbd
Stefano Novelli <murdercode@gmail.com>
2023-08-18 11:51:20 +0200

    Move check origin in middleware

--- src/Http/Controllers/TinyImageController.php
+++ src/Http/Controllers/TinyImageController.php
@@ -11,20 +11,9 @@ class TinyImageController
 {
     public function upload(Request $request): JsonResponse
     {
-
-        // Validate request
         $validator = $this->validateRequest($request);
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()->first()]);
-        }
-
-        $accepted_origins = [config('app.url')];
-        if (isset($_SERVER['HTTP_ORIGIN'])) {
-            if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
-                header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
-            } else {
-                header('HTTP/1.1 403 Origin Denied');
-            }
         }
 
         $imageFolder = config('nova-tinymce-editor.extra.upload_images.folder') ?? 'images';
@@ -34,12 +23,8 @@ class TinyImageController
             $file = Storage::disk('public')->putFile($imageFolder, $temp['tmp_name']);
 
             return response()->json(['location' => Storage::disk('public')->url($file)]);
-
         } else {
-            // Notify editor that the upload failed
-            header('HTTP/1.1 500 Server Error');
-
-            return response()->json(['error' => 'Server error']);
+            return response()->json(['error' => 'Failed to move uploaded file.']);
         }
 
     }
