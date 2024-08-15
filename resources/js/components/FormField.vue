267733f57d9480e9298def54fe9dc2971b4833d8
Pascal Lemmen <pascallemmen@gmail.com>
2024-08-15 23:13:43 +0200

    append uuid after attribute for repeater

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -3,6 +3,7 @@
                 :show-help-text="showHelpText">
     <template #field>
       <editor
+          :id="currentField.attribute + '-' + uuid "
           v-model="value"
           :api-key="currentField.options.apiKey"
           :cloud-channel="currentField.options.cloudChannel ?? 6"
@@ -20,6 +21,7 @@
 <script>
 import { DependentFormField, HandlesValidationErrors } from 'laravel-nova'
 import Editor from '@tinymce/tinymce-vue'
+import {uuid} from "@tinymce/tinymce-vue/lib/es2015/main/ts/Utils";
 
 export default {
   mixins: [DependentFormField, HandlesValidationErrors],
@@ -36,6 +38,7 @@ export default {
   },
 
   methods: {
+    uuid,
     setupProtectContent () {
       if (this.field.options.init.protect) {
         this.field.options.init.protect = this.field.options.init.protect.map((regex) => {
