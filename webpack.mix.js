30cceb7620ded64b00a6c25acb9f803d5bebf05f
Pascal Lemmen <pascallemmen@gmail.com>
2024-08-16 19:28:04 +0200

    append css to mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,7 @@ mix.disableNotifications()
 
 mix
   .setPublicPath('dist')
+  .css('resources/css/field.css', 'css')
   .js('resources/js/field.js', 'js')
   .vue({ version: 3 })
   .nova('murdercode/nova4-tinymce-editor')
