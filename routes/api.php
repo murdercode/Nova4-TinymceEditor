60d85387dd70a74f1ad21519fa528081043c7493
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2026-01-21 15:11:24 +0100

    Remove /upload api route

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Murdercode\TinymceEditor\Http\Controllers\TinyImageController;
 use Murdercode\TinymceEditor\Http\Middleware\TinymceMiddleware;
 
 // Without CSRF protection
-Route::post('/upload', TinyImageController::class)->name('tinymce.upload')
-    ->withoutMiddleware([VerifyCsrfToken::class])
-    ->middleware(TinymceMiddleware::class);
+// Route::post('/upload', TinyImageController::class)->name('tinymce.upload')
+//     ->withoutMiddleware([VerifyCsrfToken::class])
+//     ->middleware(TinymceMiddleware::class);
